3b0a77e940b440b9608d50d60b2e659703eb731f
GabeRundlett <gabe@rundlett.com>
2020-01-20 17:49:59 -0500

    Added serial library. WIP

--- src/Arduino.cpp
+++ src/Arduino.cpp
@@ -1,117 +1,89 @@
 #include "Arduino.hpp"
 
-#define DEBUG_ENABLE_TIMING
-#define DEBUG_ENABLE_LOGGING
-#include <Debug/Debug.hpp>
-
 namespace Henry {
-    namespace Platform {
-        unsigned int getDeviceCount();
-        char *getDevicePathByIndex(const unsigned int aIndex);
-    } // namespace Platform
+	static void defaultArduinoUpdate(Arduino &) {}
 
-    static void defaultArduinoUpdate(Arduino &) {}
+	Arduino::Arduino() :
+	mSerialDevice(), mTickBegin(0), mRsize(0), mTsize(0), mRdataBuffer(nullptr), mTdataBuffer(nullptr), mFlags(NONE),
+	mKey(0), mTick(0), mOnUpdate(defaultArduinoUpdate), mRdata(nullptr), mTdata(nullptr) {}
 
-    Arduino::Arduino()
-        : mSerialDevice(), mTickBegin(0), mRSize(0), mTSize(0), mRDataBuffer(nullptr), mTDataBuffer(nullptr), mFlags(NONE),
-          mKey(0), mTick(0), mOnUpdate(defaultArduinoUpdate), mRData(nullptr), mTData(nullptr) {}
-    Arduino::Arduino(const unsigned int aRSize, const unsigned int aTSize, const unsigned int aBaudrate,
-                     const unsigned char aKey)
-        : mSerialDevice(), mTickBegin(0), mRSize(aRSize + 2), mTSize(aTSize + 2), mRDataBuffer(new unsigned char[mRSize]),
-          mTDataBuffer(new unsigned char[mTSize]), mFlags(INIT_STATUS), mKey(aKey), mTick(0), mOnUpdate(defaultArduinoUpdate),
-          mRData(mRDataBuffer + 1), mTData(mTDataBuffer + 1) {
-        Connect(aRSize, aTSize, aBaudrate, aKey);
-    }
-    Arduino::~Arduino() { Disconnect(); }
+	Arduino::Arduino(const unsigned int rsize, const unsigned int tsize, const unsigned int baudrate, const unsigned char key) :
+	mSerialDevice(), mTickBegin(0), mRsize(rsize + 2), mTsize(tsize + 2), mRdataBuffer(new unsigned char[mRsize]),
+	mTdataBuffer(new unsigned char[mTsize]), mFlags(INIT_STATUS), mKey(key), mTick(0), mOnUpdate(defaultArduinoUpdate),
+	mRdata(mRdataBuffer + 1), mTdata(mTdataBuffer + 1) {
+		connect(rsize, tsize, baudrate, key);
+	}
 
-    void Arduino::Connect(const unsigned int aRSize, const unsigned int aTSize, const unsigned int aBaudrate,
-                          const unsigned char aKey) {
-        DEBUG_BEGIN_FUNC_PROFILE;
+	Arduino::~Arduino() { disconnect(); }
 
-        if (!(mFlags & INIT_STATUS)) {
-            mRSize = aRSize + 2, mTSize = aTSize + 2, mKey = aKey;
-            mRDataBuffer = new unsigned char[mRSize];
-            mTDataBuffer = new unsigned char[mRSize];
-        }
+	void Arduino::connect(const unsigned int rsize, const unsigned int tsize, const unsigned int baudrate, const unsigned char key) {
+		if (!(mFlags & INIT_STATUS)) {
+			mRsize = rsize + 2, mTsize = tsize + 2, mKey = key;
+			mRdataBuffer = new unsigned char[mRsize];
+			mTdataBuffer = new unsigned char[mRsize];
+		}
 
-        mRDataBuffer[0] = mKey, mRDataBuffer[mRSize] = mKey;
-        for (mRData = mRDataBuffer + 1; mRData < mRDataBuffer - 1; ++mRData)
-            *mRData = 0;
-        mTDataBuffer[0] = mKey, mTDataBuffer[mTSize] = mKey;
-        for (mTData = mTDataBuffer + 1; mTData < mTDataBuffer - 1; ++mTData)
-            *mTData = 0;
-        mRData = mRDataBuffer + 1, mTData = mTDataBuffer + 1;
+		mRdataBuffer[0] = mKey, mRdataBuffer[mRsize] = mKey;
+		for (mRdata = mRdataBuffer + 1; mRdata < mRdataBuffer - 1; ++mRdata)
+			*mRdata = 0;
+		mTdataBuffer[0] = mKey, mTdataBuffer[mTsize] = mKey;
+		for (mTdata = mTdataBuffer + 1; mTdata < mTdataBuffer - 1; ++mTdata)
+			*mTdata = 0;
+		mRdata = mRdataBuffer + 1, mTdata = mTdataBuffer + 1;
 
-        if (!mOnUpdate) mOnUpdate = defaultArduinoUpdate;
+		if (!mOnUpdate) mOnUpdate = defaultArduinoUpdate;
 
-        for (unsigned int tIndex = 0; tIndex < Platform::getDeviceCount(); ++tIndex) {
-            const char *const tFilepath = Platform::getDevicePathByIndex(tIndex);
-            if (mSerialDevice.Open(tFilepath, aBaudrate)) {
-                Debug::Log::message("Attempting to shake hands with %s...", tFilepath);
-                for (unsigned int i = 0; i < HANDSHAKE_ATTEMPTS; ++i) {
-                    mSerialDevice.Write(mTDataBuffer, mTSize);
-                    Debug::Timer::sleep(TICK_INTERVAL);
-                    mSerialDevice.Read(mRDataBuffer, mRSize);
-                    Debug::Timer::sleep(TICK_INTERVAL);
+		for (unsigned int i = 0; i < Platform::getDeviceCount(); ++i) {
+			const char *const tFilepath = Platform::getDevicePathByIndex(i);
+			if (mSerialDevice.open(tFilepath, baudrate)) {
+				for (unsigned int j = 0; j < HANDSHAKE_ATTEMPTS; ++j) {
+					mSerialDevice.writeBuffer(mTdataBuffer, mTsize);
+					// Debug::Timer::sleep(TICK_INTERVAL);
+					mSerialDevice.readBuffer(mRdataBuffer, mRsize);
+					// Debug::Timer::sleep(TICK_INTERVAL);
+					if (mRdataBuffer[0] == mKey && mRdataBuffer[mRsize - 1] == mKey) {
+						// mTickBegin = Debug::Timer::now();
+						mFlags |= CONNECTION_STATUS;
+						return;
+					}
+				}
+				mSerialDevice.close();
+			}
+		}
+		mFlags &= ~CONNECTION_STATUS;
+	}
 
-                    for (unsigned int i = 0; i < mRSize; ++i)
-                        Debug::Log::info("%0*x ", 2, mRDataBuffer[i]);
-                    Debug::Log::info(": %0*x\n", 2, mKey);
+	void Arduino::disconnect() {
+		if (isConnected()) {
+			if (mRdataBuffer) delete[] mRdataBuffer;
+			if (mTdataBuffer) delete[] mTdataBuffer;
+			mRdata = nullptr, mRdataBuffer = nullptr;
+			mTdata = nullptr, mTdataBuffer = nullptr;
+			mFlags = NONE;
+			mSerialDevice.close();
+		}
+	}
 
-                    if (mRDataBuffer[0] == mKey && mRDataBuffer[mRSize - 1] == mKey) {
-                        Debug::Log::success("Opened arduino '%s' at %d baud, using key '%x'.", tFilepath, aBaudrate, mKey);
-                        mTickBegin = Debug::Timer::now();
-                        mFlags |= CONNECTION_STATUS;
-                        return;
-                    }
-                }
-                mSerialDevice.Close();
-            }
-        }
-        mFlags &= ~CONNECTION_STATUS;
-        Debug::Log::error("Failed to find arduino at %d baud with the key '%x'", aBaudrate, mKey);
-    }
-
-    void Arduino::Disconnect() {
-        DEBUG_BEGIN_FUNC_PROFILE;
-
-        if (IsConnected()) {
-            if (mRDataBuffer) delete mRDataBuffer;
-            if (mTDataBuffer) delete mTDataBuffer;
-            mRData = nullptr, mRDataBuffer = nullptr;
-            mTData = nullptr, mTDataBuffer = nullptr;
-            mFlags = NONE;
-            mSerialDevice.Close();
-        }
-    }
-
-    void Arduino::Update() {
-        const double tCurrentTime = Debug::Timer::now();
-        if (tCurrentTime - mTickBegin > TICK_INTERVAL) {
-            DEBUG_BEGIN_FUNC_PROFILE;
-
-            switch (mTick) {
-            case 0:
-                mSerialDevice.Write(mTDataBuffer, mTSize);
-                mTick = 1;
-                break;
-            case 1:
-                mSerialDevice.Read(mRDataBuffer, mRSize);
-                if (mRDataBuffer[0] == mKey && mRDataBuffer[mRSize - 1] == mKey) {
-                    // mTickBegin = Debug::Timer::now();
-                    mFlags |= VALID_STATUS;
-                } else {
-                    Debug::Log::error("failed to find the key (%x) when reading the device.", mKey);
-                    mFlags &= ~VALID_STATUS;
-                }
-                DEBUG_BEGIN_PROFILE(User_defined_update_function);
-                mOnUpdate(*this);
-                DEBUG_END_PROFILE(User_defined_update_function);
-                mTick = 0;
-                break;
-            }
-
-            mTickBegin += TICK_INTERVAL;
-        }
-    }
+	void Arduino::update() {
+		const double tCurrentTime = 0; //Debug::Timer::now();
+		if (tCurrentTime - mTickBegin > TICK_INTERVAL) {
+			switch (mTick) {
+			case 0:
+				mSerialDevice.writeBuffer(mTdataBuffer, mTsize);
+				mTick = 1;
+				break;
+			case 1:
+				mSerialDevice.readBuffer(mRdataBuffer, mRsize);
+				if (mRdataBuffer[0] == mKey && mRdataBuffer[mRsize - 1] == mKey) {
+					mFlags |= VALID_STATUS;
+				} else {
+					mFlags &= ~VALID_STATUS;
+				}
+				mOnUpdate(*this);
+				mTick = 0;
+				break;
+			}
+			mTickBegin += TICK_INTERVAL;
+		}
+	}
 } // namespace Henry
--- src/Arduino.hpp
+++ src/Arduino.hpp
@@ -1,36 +1,49 @@
 #pragma once
 
-#include <Henry/Serial.hpp>
+#include <Serial.hpp>
 
 namespace Henry {
-    struct Arduino {
-      private:
-        static constexpr unsigned int TICKS_PER_SECOND = 100;
-        static constexpr double TICK_INTERVAL = 1.0 / TICKS_PER_SECOND;
-        static constexpr unsigned int HANDSHAKE_ATTEMPTS = 10;
-        enum Flags { NONE = 0, INIT_STATUS = 1, CONNECTION_STATUS = 2, VALID_STATUS = 4 };
+	namespace Platform {
+		unsigned int getDeviceCount();
+		char *getDevicePathByIndex(const unsigned int index);
+	} // namespace Platform
 
-        Serial mSerialDevice;
-        double mTickBegin;
-        unsigned int mRSize, mTSize;
-        unsigned char *mRDataBuffer, *mTDataBuffer;
-        unsigned short mFlags;
-        unsigned char mKey, mTick;
-        void (*mOnUpdate)(Arduino &);
+	struct Arduino {
+	  private:
+		static constexpr unsigned int TICKS_PER_SECOND = 100;
+		static constexpr double TICK_INTERVAL = 1.0 / TICKS_PER_SECOND;
+		static constexpr unsigned int HANDSHAKE_ATTEMPTS = 10;
+		enum Flags {
+			NONE = 0,
+			INIT_STATUS = 1,
+			CONNECTION_STATUS = 2,
+			VALID_STATUS = 4
+		};
 
-      public:
-        unsigned char *mRData, *mTData;
+		SerialDevice mSerialDevice;
+		double mTickBegin;
+		unsigned int mRsize, mTsize;
+		unsigned char *mRdataBuffer, *mTdataBuffer;
+		unsigned short mFlags;
+		unsigned char mKey, mTick;
+		void (*mOnUpdate)(Arduino &);
 
-        Arduino();
-        Arduino(const unsigned int aRSize, const unsigned int aTSize, const unsigned int aBaudrate, const unsigned char aKey);
-        ~Arduino();
+	  public:
+		unsigned char *mRdata, *mTdata;
 
-        void Connect(const unsigned int aRSize, const unsigned int aTSize, const unsigned int aBaudrate,
-                     const unsigned char aKey);
-        void Disconnect();
-        void Update();
-        inline void OnUpdate(void (*const aFunc)(Arduino &)) { mOnUpdate = aFunc; }
-        inline bool IsConnected() const { return mFlags & CONNECTION_STATUS; }
-        inline bool IsValid() const { return mFlags & VALID_STATUS; }
-    };
+		Arduino();
+		Arduino(const unsigned int rsize, const unsigned int tsize, const unsigned int baudrate, const unsigned char key);
+		Arduino(const Arduino &a) = delete;
+		Arduino(Arduino &&a) = delete;
+		~Arduino();
+
+		void connect(const unsigned int rsize, const unsigned int tsize, const unsigned int baudrate, const unsigned char key);
+		void disconnect();
+		void update();
+		inline void onUpdate(void (*const f)(Arduino &)) { mOnUpdate = f; }
+		inline bool isConnected() const { return mFlags & CONNECTION_STATUS; }
+		inline bool isValid() const { return mFlags & VALID_STATUS; }
+		void operator=(const Arduino &a) = delete;
+		void operator=(Arduino &&a) = delete;
+	};
 } // namespace Henry
